4d85fd64ea80d02659f5fa889b08181dcfefbf5d
Denis O <denis@universal-development.com>
2016-09-08 23:10:59 +0300

    Porting test app

---
+++ android-test-app/src/main/java/com/unidev/app/testapp/activity/MainFragment.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) 2015,2016 Denis O <denis@universal-development.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.unidev.app.testapp.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.unidev.app.testapp.R;
+import com.unidev.app.testapp.core.Core;
+import com.unidev.polydata.domain.Poly;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main, container, false);
+
+        ListView listView = (ListView) view.findViewById(R.id.list);
+
+        final List<? extends Poly> records = new ArrayList<>(Core.getInstance().fetchStorage().list());
+
+
+        listView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return records.size();
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return position;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View view, ViewGroup parent) {
+                if (view == null) {
+                    view = inflater.inflate(R.layout.list_item, null);
+                }
+
+                final Poly poly = records.get(position);
+
+                TextView item = (TextView) view.findViewById(R.id.item);
+                item.setText(poly + "");
+
+                final Button button = (Button) view.findViewById(R.id.favs);
+                updateButton(button, poly);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (Core.getInstance().fetchFavorites().hasPoly(poly._id())) {
+                            Core.getInstance().fetchFavorites().removePoly(poly._id());
+                        } else {
+                            Core.getInstance().fetchFavorites().addPoly(poly);
+                        }
+                        updateButton(button, poly);
+                    }
+                });
+
+                return view;
+            }
+        });
+        return view;
+    }
+
+
+    protected void updateButton(Button button, Poly poly) {
+        if (Core.getInstance().fetchFavorites().hasPoly(poly._id())) {
+            button.setText("Remove");
+        } else {
+            button.setText("Add");
+        }
+    }
+
+}
